86bf4c0b18a2c3a52adf9258898abee82a3fe230
Sooraj-Rao <143273952+Sooraj-Rao@users.noreply.github.com>
2024-04-18 22:01:47 +0530

    #KIUYTREK87

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,5 +1,5 @@
-import { type ProjectCardProps } from "@/components/projects/project-card";
-import { type ProjectShowcaseListItem } from "@/components/projects/project-showcase-list";
+import { type ProjectCardProps } from "@/component/projects/project-card";
+import { type ProjectShowcaseListItem } from "@/component/projects/project-showcase-list";
 // import { siteMetadata } from "@/data/siteMetaData.mjs";
 
 export const PROJECT_SHOWCASE: ProjectShowcaseListItem[] = [
