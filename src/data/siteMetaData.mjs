8491a23ec55394e8da6476bbed40a5fa296f8ece
Sooraj-Rao <143273952+Sooraj-Rao@users.noreply.github.com>
2024-06-25 17:19:39 +0530

    (update:META)

--- src/data/siteMetaData.mjs
+++ src/data/siteMetaData.mjs
@@ -1,23 +1,24 @@
 export const siteMetadata = {
-  title: "Sooraj Rao portfolio",
+  title: "Sooraj Rao Portfolio",
   author: "Sooraj Rao",
   headerTitle: "Sooraj Rao",
-  description: "Web and Apps developer",
-  language: "en-us",
+  description: "Web and Apps Developer",
+  language: "en-US",
   location:
     "https://www.google.com/maps/place/Mangaluru,+Karnataka/@12.9231466,74.7696613,12z/data=!3m1!4b1!4m6!3m5!1s0x3ba35a4c37bf488f:0x827bbc7a74fcfe64!8m2!3d12.9141417!4d74.8559568!16zL20vMDFzdjZr?entry=ttu",
   contentText:
-    "About Me, React Developer, Frontend Developer, Web Developer, JavaScript, HTML, CSS, Professional Journey, Skills, Passion for Web Development,Sooraj Rao",
+    "About Me, React Developer, Frontend Developer, Web Developer, JavaScript, HTML, CSS, Professional Journey, Skills, Passion for Web Development, Sooraj Rao,Sooraj Rao.in,Sooraj Dev,Sooraj Developer,Sooraj Linkedin,Sooraj Rao Linkedin,Sooraj Rao Engineer,Sooraj Rao Projects,Sooraj Rao Mangalore,Sooraj Rao Karanataka , Sooraj Rao Dev, Sooraj Rao Web, Sooraj Rao Apps, Sooraj Rao Web and Apps, Portfolio, Projects, Tech Enthusiast, Software Engineer, UI/UX Design, Responsive Web Design, Full Stack Development, Web Performance Optimization, Cross-platform Development, Mobile Apps, Freelance Developer, Open Source Contributor, React JS, Next.js, Node.js, Express.js, MongoDB, SQL, GraphQL, REST APIs, Server-side Rendering, Client-side Rendering, Single Page Applications, Progressive Web Apps, E-commerce Websites, Blogging Platforms, Content Management Systems, Personal Websites, Portfolio Websites, Web Accessibility, SEO Optimization, Performance Tuning, Test-driven Development, Agile Methodologies, Version Control (Git), Continuous Integration / Continuous Deployment (CI/CD), AWS, Azure, Google Cloud Platform",
   theme: "light",
-  siteName: "soorajrao.xyz",
-  siteUrl: "https://soorajrao.xyz",
+  siteName: "soorajrao.in",
+  siteUrl: "https://soorajrao.in",
   siteRepo: "https://github.com/Sooraj-Rao/portfolio",
   siteLogo: "/favicon.ico",
   email: "soorajrao.in@gmail.com",
   github: "https://github.com/Sooraj-Rao",
-  twitter: "https://twitter.com/Sooraj__Rao",
+  twitter: "https://x.com/SoorajRaoo",
   linkedin: "https://www.linkedin.com/in/sooraj-rao/",
   locale: "en-US",
-  resume:"https://resume.soorajrao.in/",
-  contactForm:'https://contact.soorajrao.in/?mode=contact&app=main&utm_source=main'
+  resume: "https://resume.soorajrao.in/",
+  contactForm:
+    "https://contact.soorajrao.in/?mode=contact&app=main&utm_source=main",
 };
